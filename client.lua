e19ee851baeb27df6752c1a706b7b6e31011f17b
havockatie <havockatie@gmail.com>
2021-02-26 18:14:02 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,7 @@
 RegisterCommand("radio", function(source, args, rawCommand)
     if #args == 1 then
-        -- TriggerEvent('chatMessage', "RADIO", {255, 255, 0}, "Set radio channel to " .. args[1])
         exports.saltychat:SetRadioChannel(args[1], true)
-            r = exports.saltychat:GetRadioChannel(true)
+        r = exports.saltychat:GetRadioChannel(true)
         TriggerEvent('chatMessage', "RADIO", {255, 255, 0}, "Set radio channel to " .. r)
     end
 end)
