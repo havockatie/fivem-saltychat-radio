6bda57d59fca8dc093e4a1d4289222c65e34871a
havockatie <havockatie@gmail.com>
2021-02-26 20:20:40 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,13 @@
 RegisterCommand("radio", function(source, args, rawCommand)
+    print(#args)
     if #args == 1 then
         exports.saltychat:SetRadioChannel(args[1], true)
-        r = exports.saltychat:GetRadioChannel(true)
-        TriggerEvent('chatMessage', "RADIO", {255, 255, 0}, "Set radio channel to " .. r)
+        TriggerEvent('chatMessage', "RADIO", {255, 255, 0}, "Joined radio channel " .. args[1])
+    elseif #args == 0 then
+        channel = exports.saltychat:GetRadioChannel(true)        
+        if channel ~= nil then            
+            exports.saltychat:SetRadioChannel('', true)
+            TriggerEvent('chatMessage', "RADIO", {255, 255, 0}, "Left radio channel " .. channel)
+        end
     end
 end)
